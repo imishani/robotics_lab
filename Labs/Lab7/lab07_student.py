7ee449aa40059e666efcd17f84043fd9104a2f87
osheraz <osherazulay@mail.tau.ac.il>
2022-04-21 10:07:31 +0300

    lab_07 update

--- Labs/Lab7/lab07_student.py
+++ Labs/Lab7/lab07_student.py
@@ -9,7 +9,7 @@ def calculate_error(t_curr, R_curr, target_feature_t, target_feature_R, translat
     Calculate error based on the input pose and the target pose
     Input:  (object in current camera frame)
             t_input, 1x3 vector
-            R_input, 1x4 vector, quaternion
+            R_input, 3x3 matrix
     Output: Error, [t_err, R_err], 6x1
     '''
 
@@ -28,13 +28,12 @@ def calculate_error(t_curr, R_curr, target_feature_t, target_feature_R, translat
 
     return error
 
-
 def feature_jacobian(t_curr, R_curr, target_feature_R):
     '''
     form interaction matrix / feature jacobian base on current camera pose
     Input:  (object in current camera frame)
             t_input, 1x3 vector
-            R_input, 1x4 vector, quaternion
+            R_input, 3x3 matrix
     Output: Interation Matrix (feature Jacobian), 6x6
     '''
 
@@ -47,7 +46,7 @@ def control(L, error, _lambda):
     calculate the twist of camera frame required to reach target pose
     Input:  (object in current camera frame)
             t_input, 1x3 vector
-            R_input, 1x4 vector, quaternion
+            R_input, 3x3 matrix
     Output: Twist in camera frame
             [nu_c, omg_c], 1x6
     '''
